04ba345344ae402724a44c1979bc2dfde312465b
Robert Young <david41596@yahoo.com>
2025-09-07 14:33:36 -0400

    Removed the fake user from "User" file to make it use the Auth'd user.

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -3,17 +3,10 @@ import styles from "./User.module.css";
 import { useAuth } from "../contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
 
-const FAKE_USER = {
-  name: "Jack",
-  email: "jack@example.com",
-  password: "qwerty",
-  avatar: "https://i.pravatar.cc/100?u=zz",
-};
-
 function User() {
-  const user = FAKE_USER;
-  const { isAuthenticated, logout } = useAuth();
+  const { isAuthenticated, logout, user } = useAuth();
   const navigate = useNavigate();
+
   useEffect(
     function () {
       if (!isAuthenticated) {
